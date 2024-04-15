c9476d61ac483aaad4693a8c18ed01ffab7ade31
kkirpa <kirpa@vt.edu>
2024-04-15 17:40:30 -0400

    Money Rain
    IP

---
+++ Assets/sirenSound.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sirenSound : MonoBehaviour
+{
+    
+	public void Code (){
+		GetComponent<AudioSource>().Play();
+	}
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
