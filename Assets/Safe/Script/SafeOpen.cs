a18b7aa1297fbdd0eef2ed90581fec2feded35a5
kkirpa <kirpa@vt.edu>
2024-04-17 02:11:55 -0400

    Safe Interaction
    Trigger currently set to space bar, will attach to the keypad tmr

---
+++ Assets/Safe/Script/SafeOpen.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeOpen : MonoBehaviour
+{
+    //reference to the animator component of the safe
+    [SerializeField]
+    private Animator _anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //trigger - space key for now
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _anim.SetTrigger("0000");
+        }
+    }
+
+    // private void OnMouseDown()
+    // {
+    //     _anim.SetTrigger("0000");
+    // }
+}
